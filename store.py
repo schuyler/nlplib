b3981b57653c23634f83578fa9432a54031b2b17
Schuyler Erle <sderle@goldman.(none)>
2009-06-04 23:36:04 -0400

    Make store model actually work.

--- store.py
+++ store.py
@@ -5,21 +5,22 @@ class BaseObject (SQLObject):
     _cacheValues = False
 
 class Document (BaseObject):
-    title = UnicodeCol(notNull=1, validator=validators.String(strip_spaces=1))
-    description = UnicodeCol(notNull=1, validator=validators.String(strip_spaces=1))
-    url = StringCol(length=255, notNull=1, validator=validators.String(strip_spaces=1))
+    title = UnicodeCol(notNull=1)
+    description = UnicodeCol(notNull=1)
+    url = StringCol(length=255, notNull=1)
     source = StringCol(length=255, notNull=1)
     content_type = StringCol(length=255)
-    tags = UnicodeCol(notNull=1, validator=validators.String(strip_spaces=1))
+    tags = UnicodeCol(notNull=1)
     vector = BLOBCol()
     revision = IntCol(notNull=1)
-    current = BooleanCol(notNull=1)
+    current = BoolCol(notNull=1)
     created_at = TimestampCol(default=None)
     url_idx = DatabaseIndex('url')
     content_type_idx = DatabaseIndex('content_type')
     source_idx = DatabaseIndex('source')
 
-if __name__ = "__main__":
+if __name__ == "__main__":
+    import sys
     sqlhub.processConnection = connectionForURI("sqlite:" + sys.argv[1])
     Document.createTable()
 
